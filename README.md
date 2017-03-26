c1185520fc88b6b4bf0a17939b3de9a57d66865c
Akihiko Tomokane <mpc9000@gmail.com>
2017-03-26 22:11:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+OpenGL & Go Tutorial Part 1: Hello,  OpenGL
+=============================================
+
+https://kylewbanks.com/blog/tutorial-opengl-with-golang-part-1-hello-opengl
+
+```bash
+$ go run *.go
+```
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+const (
+	width  = 500
+	height = 500
+)
+
+var (
+	trianble = []float32{
+		0, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+	}
+
+	vertexShaderSource = `
+    #version 410
+    in vec3 vp;
+    void main() {
+        gl_Position = vec4(vp, 1.0);
+    }
+` + "\x00"
+
+	fragmentShaderSource = `
+    #version 410
+    out vec4 frag_colour;
+    void main() {
+        frag_colour = vec4(1, 1, 1, 1);
+    }
+` + "\x00"
+)
+
+func main() {
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	program := initOpenGL()
+	vao := makeVao(trianble)
+
+	for !window.ShouldClose() {
+		draw(vao, window, program)
+	}
+}
+
+// draw program
+func draw(vao uint32, window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	gl.BindVertexArray(vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(trianble)/3))
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+// initGlfw
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Conway's game of life", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+	return window
+}
+
+// init OpenGL
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func makeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
